abbd2f5ffd28cc3eb4340991fa64f95a274e4ae5
Marcin Janicki <janicki.olek77@gmail.com>
2023-03-06 22:39:02 +0100

    Major changes

--- main.py
+++ main.py
@@ -4,22 +4,20 @@ def menu_start():
         print("2.Trygonometryczna")
         print("3.Wykładnicza")
         print("4.Koniec")
-        wybor_funkcji = input("Wybierz: ")
-        wybor_funkcji = wybor_funkcji.strip()
-        if wybor_funkcji == "1" or wybor_funkcji == "2" or wybor_funkcji == "3":
+        wybor_funkcji = int(input("Wybierz: "))
+        if wybor_funkcji == 1 or wybor_funkcji == 2 or wybor_funkcji == 3:
             podstawowy_wybor()
-        elif wybor_funkcji == "4":
+        elif wybor_funkcji == 4:
             break
         else:
             print("Mierny wybór")
 
 
 def podstawowy_wybor():
-    wybor_przedzialu = input("Podaj przedział na którym poszukiwane będzie miejsce zerowe: ")
-    wybor_przedzialu = wybor_przedzialu.strip()
+    wybor_przedzialu_LEWO = int(input("Podaj lewy krawniec przedziału na którym poszukiwane będzie miejsce zerowe: "))
+    wybor_przedzialu_PRAWO = int(input("Podaj prawy kraniec przedziału na którym poszukiwane będzie miejsce zerowe: "))
     print("a.Spełnienie warunku nałożonego na dokładność")
     print("b.Osiągnięcie zadanej liczby iteracji")
-    wybor_przedzialu.strip()
     wybor_zatrzymania = input("Podaj kryterium zatrzymania algorytmu: ")
     if wybor_zatrzymania == "a":
         wybor_epsilon = input("Wprowadź ε: ")
