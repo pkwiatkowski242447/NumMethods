18fb93c2b07e19feb194c244e03cc29c05cb7f09
Aleksander Janicki <janicki.olek77@gmail.com>
2023-04-01 20:46:25 +0200

    Dodanie przykladow

--- Zadanie_2/main.py
+++ Zadanie_2/main.py
@@ -1,16 +1,11 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import pandas as pd
+import numpy as np
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def read_from_file(filename):
+    input = np.loadtxt(filename, dtype="i", delimiter=',')
+    print(input)
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    read_from_file("a.txt")
--- main.py
+++
@@ -1,4 +0,0 @@
-import menu as m
-
-if __name__ == '__main__':
-    m.menu_start()
